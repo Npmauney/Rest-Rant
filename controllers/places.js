3350934e4f7470b35459af5cc90373eeb5e5dd31
Npmauney <nathanielmauney@gmail.com>
2023-07-14 18:50:19 -0400

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,12 +1,18 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
+// GET /places
+router.get('/', (req, res) => {
+    
+  res.render('places/index', { places })
+})
 
-
+//GET /new
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+//GET show page
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -16,11 +22,27 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
+  }
+})
+
+//DELETE
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
 
 
+//POST new place
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
@@ -39,9 +61,5 @@ router.post('/', (req, res) => {
 
 
 
-// GET /places
-router.get('/', (req, res) => {
-    
-    res.render('places/index', { places })
-})
+
 module.exports = router
