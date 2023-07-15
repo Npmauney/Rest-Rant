4369d1f265a70bb5be7c3fe947258d0c9fd762e5
Npmauney <nathanielmauney@gmail.com>
2023-07-15 14:02:23 -0400

    Added edit functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
           <main>
             <h1>Edit Place</h1>
-            <form method='POST' action={`/places/${data.place.id}?_method=PUT`}>
+            <form method='POST' action={`/places/${data.id}?_method=PUT`}>
                 <div className='row'>
                     <div className='form group col-sm-6'>
                         <label htmlFor='name'>Place Name</label>
@@ -31,7 +31,7 @@ function edit_form (data) {
                         <label htmlFor='cuisines'>Cuisines</label>
                         <input className='form-control' id='cuisines' name='cuisines' required/>
                     </div>
-                    <input className='btn btn-primary' type='submit' value='Add place' />
+                    <input className='btn btn-primary' type='submit' value='Update place' />
                 </form>
           </main>
         </Def>
