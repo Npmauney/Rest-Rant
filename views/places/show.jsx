0c71c89b7f5b3a42c28af837e62f51bf7175fa21
Npmauney <nathanielmauney@gmail.com>
2023-07-22 13:40:12 -0400

    imported seeders

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,23 +5,25 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <h2>Rating</h2>
-            <h4>Not yet Rated</h4>
-            <br/>
-            <h2>Description</h2>
-            <h4>To be filled</h4>
-            <br/>
-            <h2>Comments</h2>
-            <h4>No comments yet!</h4>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>
-            <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-              <button type='submit' className='btn btn-danger'>
-                Delete
-              </button>
-            </form> 
+            <div className='row'>
+              <h1>{ data.place.name }</h1>
+              <h2>Rating</h2>
+              <h4>Not yet Rated</h4>
+              <br/>
+              <h2>Description</h2>
+              <h4>To be filled</h4>
+              <br/>
+              <h2>Comments</h2>
+              <h4>No comments yet!</h4>
+              <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                  Edit
+              </a>
+              <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                <button type='submit' className='btn btn-danger'>
+                  Delete
+                </button>
+              </form>
+            </div> 
           </main>
         </Def>
     )
