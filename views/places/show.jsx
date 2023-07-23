cd9f71f2421542aa18c0cf0c0cb6e4a2540de5ef
Npmauney <nathanielmauney@gmail.com>
2023-07-23 13:35:42 -0400

    comments now displayed

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+      <h3 className='inactive'>
+        No comments yet!
+      </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className='border'>
+            <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <strong>- {c.author}</strong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
           <main>
@@ -24,7 +43,7 @@ function show (data) {
                 </h4>
                 <br/>
                 <h2>Comments</h2>
-                <h4>No comments yet!</h4>
+                {comments}
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                     Edit
                 </a>
